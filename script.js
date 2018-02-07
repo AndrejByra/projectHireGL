8378e689a848a28996c6c712934ed42434f63dbe
Andrej <andrej.byra@centrum.sk>
2018-02-07 22:17:28 +0100

    added new file main.html and added ajax connection

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 $(document).ready(function()
 {
-	
-	$("#errormessage").hide();
+	$("#errormessage").empty();
 
 	$("#btn").click(function()
 	{
-		var login=$("#entername").val();
+		var username=$("#entername").val();
 		var password=$("#enterpassword").val();
-		if (login.length<2 || password.length<2)
-			$("#errormessage").show();
+		if (username.length<2 || password.length<2)
+		{
+			$("#errormessage").text('Username and password must have at least 2 characters');
+		}
 		else
-			$("#errormessage").hide();
+			$("#errormessage").empty();
+
+		$.ajax
+		({
+			url: 'http://akademiasovy.ddns.net:8081/api/auth/login',
+			type: 'POST',
+			contentType: 'application/json',
+
+			data: JSON.stringify
+			({
+				"userName": username,
+				"password": password
+			}),
+
+			success: function(data)
+			{
+				window.location.href='main.html';
+			},
+
+			error: function()
+			{
+				$("#errormessage").empty();
+				$("#errormessage").html('Invalid Username or Password.<br>Please try again.');
+			}
+		});
+		
 	});
 });
